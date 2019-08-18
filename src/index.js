6c84a9e84e69724bd84a9d42b4e85827e6d1a4c2
David <david.shams@gmail.com>
2019-08-18 14:59:54 -0500

    BUG: Clear error message - read Check your code at Home.js:12.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from './components/home/Home.js';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Hello() {
     return <div>Hello React Redux
+        {/* <Home /> */}
         <Home />
     </div>
 }
