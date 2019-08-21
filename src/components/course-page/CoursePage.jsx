f332a41c20e6b11dcd546b4de9e298fe4ad8390e
David <david.shams@gmail.com>
2019-08-21 09:11:17 -0500

    Connect CoursePage component to redux

--- src/components/course-page/CoursePage.jsx
+++ src/components/course-page/CoursePage.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default class CoursePage extends Component {
 
@@ -41,3 +42,8 @@ export default class CoursePage extends Component {
         )
     }
 }
+
+/**
+ * Connect CoursePage component to redux
+ */
+function mapStateToProps()
