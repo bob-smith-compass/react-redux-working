e9a9a2b29935892102518a3a8b3b2e16070ebabf
David <david.shams@gmail.com>
2019-08-24 11:53:57 -0500

    DEBUG: for for(let i=0; i<howMany; i++){

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+// import { React, useState } from "react";
 
-const Button = () => {
-    return (
-        <div>
-            <button>RButton</button>
-        </div>
-    );
+// const Button = () => {
+//   const [count, setCount] = useState(9);
+
+//   return (
+//     <div>
+//       <button>{`RButton ${count}`}</button>
+//     </div>
+//   );
+// };
+
+// export default Button;
+
+import { React, useState } from "react";
+
+
+const Display = (props) => {
+  return (
+    <div onClick={props.onClickFunctionComesFromParent}>
+      {props.count}
+    </div>
+  );
+};
+
+
+
+const Button = (props) => {
+  const [count, setCount] = useState(9);
+  let handleClick = () => {
+    setCount(count+props.incBy);
+    console.log('handleClick '+count);
+  }
+  return (
+    <div>
+
+      <Display count={count} onClickFunctionComesFromParent={handleClick}/>
+      <button onClick={handleClick}>{`RButton ${count}`}
+      <Display count={count} />
+        
+      </button>
+      <button onClick={handleClick}>{`RButton ${count}`}</button>
+    </div>
+  );
+};
+export default Button;
+const List = () => {
+  let howMany = 5;
+  return(
+    <div>
+      {
+        // for(let i=0; i<howMany; i++){
+          // return i;
+          (<Button incBy={15} />)
+        // } 
+      }
+    </div>
+                    
+  )
 }
 
-export default Button;
+// ReactDOM.render(<List />, mountNode);
