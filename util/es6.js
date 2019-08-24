e095a3c7f761b80e8a0b782c1f4fa0c9cec7d974
David <david.shams@gmail.com>
2019-08-24 13:29:50 -0500

    Babel MAY THROUGH ERROR and may look like syntax error while nothing wrong - Check babel sittings

--- util/es6.js
+++ util/es6.js
@@ -1,3 +1,28 @@
 let [first, ...others] = [1, 2,3,4,5];
 
 console.log(first, others); // 1 [ 2, 3, 4, 5 ]
+
+class Person {
+    
+    constructor(name = 'David'){
+        this.name = name;
+    };
+    show() {
+        console.log(`Hell Mr. ${this.name}`);
+    }
+}
+
+class Employee extends Person {
+    constructor(name, SSN = 0){
+      super(name);
+      this.SSN = SSN;
+    };
+    show() {
+        console.log(`Hello Mr. ${this.name}. You'r SSN is ${this.SSN}`);
+    }
+}
+let p = new Person('John');
+let david = new Employee(12345678);
+
+p.show();
+david.show();
